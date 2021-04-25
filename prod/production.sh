aa63d7f01bf1835002e707de01870fe5cbf98565
vitolg <vitolg@hotmail.com>
2021-04-25 01:22:40 -0400

    Update production.sh

--- prod/production.sh
+++ prod/production.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Getting latest data...."
-python ~/DEV/ds/advanced/covid_ont/prod/getdata.py
+python covid_ont/prod/getdata.py
 echo "Making images...."
-python ~/DEV/ds/advanced/covid_ont/prod/vaccases.py
-python ~/DEV/ds/advanced/covid_ont/prod/byunit.py
+python covid_ont/prod/vaccases.py
+python covid_ont/prod/byunit.py
 echo "Deploying...."
 firebase deploy
 echo "Done."
