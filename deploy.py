ea996eca9f40e67bebfe0a2fde3618a1e653c4aa
vitolg <vitolg@hotmail.com>
2021-04-26 10:29:57 -0400

    corrected deploy.py: removed shell option

--- deploy.py
+++ deploy.py
@@ -2,11 +2,11 @@ import subprocess
 from covid_ont import DIR_LOG
 
 def deploy():
-    print('Downloading date...')
+    print('Downloading data...')
     from covid_ont.py import getdata
     print('Making plots...')
     from covid_ont.py import vaccases
     from covid_ont.py import byunit
     print('Deploying...')
-    subprocess.run(['firebase', 'deploy'], shell=True)
+    subprocess.run(['firebase', 'deploy'])
     print('Done.')
