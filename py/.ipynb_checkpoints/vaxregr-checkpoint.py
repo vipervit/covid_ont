aef07a7e79bab87131e508e3135c1debb1ab5f6b
Viper Vit <vitolg1@gmail.com>
2021-05-09 13:28:11 -0400

    1.15.03: vaxreg.py corrected

--- py/.ipynb_checkpoints/vaxregr-checkpoint.py
+++ py/.ipynb_checkpoints/vaxregr-checkpoint.py
@@ -20,7 +20,7 @@ sys.path.append(os.getenv('SITES_HOME'))
 import covid_ont
 from covid_ont import dataset_read, dataset_get, DIR_IMAGES, FIGSIZES
 
-f_plot='vaxxregr.png'
+f_plot='vaxregr.png'
 
 ontario_pop_estimate=14755211 # Q1 2021, source: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901
 forecast_depth=300
@@ -39,7 +39,7 @@ df['Percentage of Population Fully Vaccinated'] = df['Total Fully Vaccinated'].a
 
 df_future=df.append(pd.DataFrame([[0,0,0,0,0,0] for i in range(forecast_depth)], index=pd.date_range(start=df.index.max(), periods=forecast_depth, freq='D'), columns=df.columns))
 
-X_act=np.reshape(df.index, (-1,1)) 
+X_act=np.reshape(df.index, (-1,1))
 y_act=df['Percentage of Population Vaccinated with One Dose']
 
 X_train, X_test, y_train, y_test = train_test_split(X_act, y_act, test_size=0.8, random_state=42)
@@ -56,7 +56,7 @@ X_pred=np.reshape(df_pred.index, (-1,1))
 y_pred=pipe.predict(X_pred)
 
 df_pred['Percentage of Population Vaccinated with One Dose']=y_pred
-df_pred=df_pred[df_pred['Percentage of Population Vaccinated with One Dose']<100]
+df_pred=df_pred[df_pred['Percentage of Population Vaccinated with One Dose']<101]
 df_plot_pred=df_pred.copy()
 df_plot_pred.columns=['Projection']
 
@@ -90,4 +90,3 @@ ax.hlines(peak['Projection'], df_plot_act.index.min(), df_plot_pred.index.max(),
 plt.vlines(today.index[0], today['Actual'], 0, linestyles='dashed', color='red')
 
 plt.savefig(DIR_IMAGES + f_plot, facecolor='oldlace')
-
